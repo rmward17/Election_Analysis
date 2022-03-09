bed0ab994a200afdb6d53ab113d61e2627f39bf0
rmward17 <wardrachelm17@gmail.com>
2022-03-08 19:04:13 -0500

    commiting changes to PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -10,6 +10,12 @@ import csv
 # Assign a variable for the file to load and the path
 file_to_load = os.path.join("Resources",'election_results.csv')
 
+# Initialize a total vote counter
+total_votes = 0
+
+# candidate options
+candidate_options = []
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -18,11 +24,24 @@ with open(file_to_load) as election_data:
      file_reader = csv.reader(election_data)
      # print the header rows
      headers = next(file_reader)
-     print(headers)
      
      # print each row in the CSV file
-     #for row in file_reader:
-       #  print(row)
+     for row in file_reader:
+          # Add to the total vote vount
+         total_votes += 1
+          # Pull the candidate name from each row
+         candidate_name = row[2]
+         # If the candidate does not match any existing candidate...
+         if candidate_name not in candidate_options:
+           # Add it to the list of candidates
+           candidate_options.append(candidate_name)
+
+        
+# Print the total votes
+print(total_votes)
+
+# Print the candidate list
+print(candidate_options)
 
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
