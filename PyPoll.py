0dd3de3a21448b3e1c65762ffa8b824ed494f5b7
rmward17 <wardrachelm17@gmail.com>
2022-03-08 21:21:26 -0500

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -11,6 +11,7 @@ import csv
 file_to_load = os.path.join("Resources",'election_results.csv')
 
 # Initialize a total vote counter
+# Must initialize variables, lists, and dictionaries outside of opening the file 
 total_votes = 0
 
 # candidate options
@@ -18,6 +19,11 @@ candidate_options = []
 
 # candidate votes
 candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
@@ -40,7 +46,6 @@ with open(file_to_load) as election_data:
            candidate_options.append(candidate_name)
            # dictionary_name[key] = value 
            candidate_votes[candidate_name] = 0
-      #increment each candidate name
       candidate_votes[candidate_name] += 1
 
 # Determine percentage of votes for each candidate by looping thorugh the counts
@@ -52,9 +57,25 @@ for candidate_name in candidate_votes:
   votes = candidate_votes[candidate_name]
 # Calculate the percentage of the vote count for each candidate
   vote_percentage = float(votes) / float(total_votes) * 100
+  print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+  if (votes > winning_count) and (vote_percentage > winning_percentage):
+    # If true then set winning_count = votes and winning_percent =
+    # vote_percentage.
+    winning_count = votes
+    winning_percentage = vote_percentage
+    # And, set the winning_candidate equal to the candidate's name.
+    winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 # print the percentages for each of the candidates
 # print ( "key: recieved {calculated value}% of the vote.")
-  print(f"{candidate_name}: recieved {vote_percentage:.1f}% of the vote.")
+# print(f"{candidate_name}: recieved {vote_percentage:.1f}% of the vote.")
 # Print the total votes
 # print(total_votes)
 
